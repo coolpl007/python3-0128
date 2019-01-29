181fca67c1a3cb98d17d5511c258b6cb46681287
coolpl007 <pengleiyeah@163.com>
2019-01-29 23:44:50 +0800

    new code

--- pl190128.py
+++ pl190128.py
@@ -7,4 +7,15 @@ for i in range(5):
 turtle.circle(100)
 turtle.done()
 
+turtle.pen()
+my_colors = ("red","green","yellow","black")
+turtle.width(10)
+turtle.speed(5)
+for i in range(10):
+    turtle.penup()
+    turtle.goto(0,-i*20)
+    turtle.pendown()
+    turtle.color(my_colors[i%len(my_colors)])
+    turtle.circle(100+i*20)
 
+turtle.done()
